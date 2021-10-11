a060fd8cc5a063fa24351c5c8bf252e180930b40
shutheonetokyo <ss1030ss@icloud.com>
2021-10-11 23:08:23 +0900

    Add a full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  
   def home
   end
 
@@ -6,8 +7,5 @@ class StaticPagesController < ApplicationController
   end
 
   def about 
-  end
-
-  def contact
   end
 end
